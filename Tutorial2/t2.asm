37d836215d06dd6279e806e95f2cb9667558644d
darts <darts@tcd.ie>
2019-10-02 14:22:35 +0100

    Update t2.asm

--- Tutorial2/t2.asm
+++ Tutorial2/t2.asm
@@ -4,43 +4,56 @@ option casemap:none             ; case sensitive
  public g
  g QWORD 4
 
+pstr db 'a = %I64d b = %I64d c = %I64d d = %I64d e = %I64d sum = %I64d\n'
+
 .code
 
-public min
-min:	
-	mov rax, rcx
-	cmp rdx, rax
-	jge chkCMin
-	mov rax, rdx
-chkCMin:
-	cmp r8, rax
-	jge retMin
-	mov rax, r8
-retMin:
-	ret
+public min					; int64 min(int64 a, int64 b, int64 c)
+min:						; {
+	mov rax, rcx			;	retNum = a
+	cmp rdx, rax			;	if(b < retNum)
+	jge chkCMin				;	{
+	mov rax, rdx			;		retNum = b
+chkCMin:					;	{
+	cmp r8, rax				;	if(c < retNum)
+	jge retMin				;	{
+	mov rax, r8				;		retNum = c
+retMin:						;	}
+	ret						; }
 
 
-public p
-p:
-	push r8
-	push r9
-	mov r8, g
-	sub rsp, 32
-	call min
-	add rsp, 32
-	mov rcx, rax
-	pop r8
-	pop rdx
-	sub rsp, 32
-	call min
-	add rsp, 32
-	ret
+public p					; int64 p(int64 i, int64 j, int64 k, int64 l)
+p:							; {
+	push r8					;	k.pushToStack()
+	push r9					;	l.pushToStack()
+	mov r8, g				;	g.load()
+	sub rsp, 32				;	allocateShadowSpace()
+	call min				;	pRes = min(g, i, j)
+	add rsp, 32				;	deallocateShadowSpace()
+	mov rcx, rax			;	pRes.moveToFunction()
+	pop r8					;	l.popFromStack()
+	pop rdx					;	k.popFromStack()
+	sub rsp, 32				;	allocateShadowSpace()
+	call min				;	result = min(pRes, l, k)
+	add rsp, 32				;	deallocateShadowSpace()
+	ret						; }
 
 
 public gcd
 gcd:
 	mov rax, rcx
+	test rdx, rdx
+	je retGCD
 
+	push rbx
+	mov rbs, rcx
+	cqo
+	idiv rcx
+	mov rcx, rbx
+	call gcd
+	pop rbx
+retGCD:
+	ret
 
 
 public q
