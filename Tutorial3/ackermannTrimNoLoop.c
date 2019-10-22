b111fc7e8e949b1426599e8616dcaa18073e3a0b
darts <darts@tcd.ie>
2019-10-22 20:33:50 +0100

    Added another file

--- Tutorial3/ackermannTrimNoLoop.c
+++ Tutorial3/ackermannTrimNoLoop.c
@@ -1,6 +1,3 @@
-// ******
-int runCount = 100000;
-// ******
 int ackermann(int x, int y)
 {
     if (x == 0)
@@ -13,13 +10,11 @@ int ackermann(int x, int y)
     }
     else
     {
-        return ackermann(x - 1, ackermann(x, y - 1));
+        return ackermann(x - 1, ackermann(x, y - 1)); 
     }
 }
 
 int main()
 {
-    for(int i = 0; i < runCount; i++){
-        ackermann(3, 6);
-    }
+ackermann(3, 6);
 }
