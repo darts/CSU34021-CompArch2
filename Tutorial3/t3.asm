010ff713ec04c392cb43f2dfb196e73e7044e8b0
darts <darts@tcd.ie>
2019-10-22 18:57:57 +0100

    Fixed and optimised gcd
    Registers were in the wrong order.
    Forgot rd is last reg in instruction.
    Also optimised gcd.

--- Tutorial3/t3.asm
+++ Tutorial3/t3.asm
@@ -20,15 +20,15 @@ min1:
 
 public p                    ; p function
 p:
-    add r10, r0, r3         ; load g to pass to function
-    add r11, r0, r26        ; load i to pass to function
+    add r3, r0, r10         ; load g to pass to function
+    add r26, r0, r11        ; load i to pass to function
     callr r25, min          ; rVal = min(g, i , j)
-    add r12, r0, r27        ; load j to pass to function
+    add r27, r0, r12        ; load j to pass to function
 
-    add r10, r0, r1         ; load rVal to pass to function
-    add r11, r0, r28        ; load k to pass to function
+    add r1, r0, r10         ; load rVal to pass to function
+    add r28, r0, r11        ; load k to pass to function
     callr r25, min          ; retVal = min(rVal, k, l)
-    add r12, r0, r29        ; load l to pass to function
+    add r29, r0, r12        ; load l to pass to function
 
     ret r25, 0              ; jmp(&r25 + 0)
     xor r0, r0, r0          ; no-op()
@@ -36,17 +36,17 @@ p:
 
 public gcd
 gcd:
-    add r26, r0, r1         ; retVal = a
     sub r27, r0, r0, {C}    ; if(b == 0)
-    jeq retGCD              ; { jmp to end and return a }
+    jeq retGCD              ; { jmp to end and return a (dont forget the next OP though)}
+    add r1, r0, r26         ; retVal = a
 
-    add r10, r0, r26        ; load a for mod function
-    add r11, r0, r27        ; load b for mod function
+    add r26, r0, r10        ; load a for mod function
     callr r25, mod          ; tmp = a % b
+    add r27, r0, r11        ; load b for mod function
 
-    add r10, r0, r27        ; load a for gcd recursive call
-    add r11, r0, r1         ; load tmp for gcd recursive call
+    add r27, r0, r10        ; load b for gcd recursive call
     callr r25, gcd          ; retVal = gcd(a, tmp) 
+    add r1, r0, r11         ; load tmp for gcd recursive call
 
     ret r25, 0              ; jmp(&r25 + 0)
     xor r0, r0, r0          ; no-op()
