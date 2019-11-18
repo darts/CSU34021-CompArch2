1f326148d4b7c2dbb96b8a54b9189b1e61eb90a3
darts <darts@tcd.ie>
2019-11-18 18:34:40 +0000

    Update cache_sim.py
    Sorta broken for K>1

--- Tutorial6/cache_sim.py
+++ Tutorial6/cache_sim.py
@@ -1,4 +1,5 @@
 import sys
+import math
 # input: cache size, x-way, bytes per line, list of memory locations in a file
 # output: "addr: HIT/MISS"
 
@@ -25,6 +26,22 @@ def insert_tag(tag, set_num):
     c_block[set_num].insert(0, tag)
     c_block[set_num].pop()
 
+def gen_masks():
+    offset_length = int(math.log2(int(sys.argv[1])))
+    setnum_length = int(math.log2(int(sys.argv[2])))
+    addr_mask = ''
+    tag_mask = ''
+    for i in range(16-(offset_length + setnum_length)):
+        addr_mask += '1'
+        tag_mask += '0'
+    for i in range(setnum_length):
+        addr_mask += '0'
+        tag_mask += '1'
+    for i in range(offset_length):
+        addr_mask += '0'
+        tag_mask += '0'
+    return [int(addr_mask,2), int(tag_mask,2)]
+
 
 if len(sys.argv) != 5:
     print(f'usage: {sys.argv[0]} L N K file_containing_addresses \n   eg: {sys.argv[0]} 16 8 1 addr.txt')
@@ -33,16 +50,17 @@ if len(sys.argv) != 5:
 with open(sys.argv[4]) as a_list:
     numbers = list(map(lambda l: int(l, 16), a_list))
 
-res_list = [len(numbers)]
+res_list = []
 
 hit_count = 0
 miss_count = 0
 
 c_block = create_block(int(sys.argv[3]), int(sys.argv[2]), -1)
+masks = gen_masks()
 
 for i in numbers:
-    addr_tag = ((i & int('111111110000000', 2)) >> 7)
-    set_num  = ((i & int('000000001110000', 2)) >> 4)
+    addr_tag = ((i & masks[0]) >> 7)
+    set_num  = ((i & masks[1]) >> 4)
 
     if lookup_tag(addr_tag, set_num):
         update_lru(addr_tag, set_num)
@@ -54,3 +72,5 @@ for i in numbers:
         miss_count += 1
 
 print(hit_count)
+for i in res_list:
+    print(i)
